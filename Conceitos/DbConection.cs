4656f3263ea4cc6bb96465a25b1bb8cfadb2d1ee
BOSS <BOSS@DESKTOP-67IPUO7>
2025-06-21 20:49:27 -0300

    Adicionar arquivos de projeto.

---
+++ Conceitos/DbConection.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Conceitos
+{
+    class DbConection
+    {
+        // String de conexão
+
+        private readonly string _dbConection;
+
+        //Atributos de crendencias e parametros pasra conexão
+
+        private const string _server = "localhost";
+        private const string _base = "dbbase";
+        private const string _user = "root";
+        private const string _password = "Admin";
+
+        //Construtor da conexão
+
+        public DbConection()
+        {
+            _dbConection = $"Server={_server};Database={_base};User ID={_user};PassWord={_password};";
+        }
+
+        //Metodo retorno da conexão
+        public string RetornarConexão()
+        {
+            return _dbConection;
+        }
+
+        //Testar Conexão
+
+        public void TestConnection()
+        {
+            try
+            {
+                using var connection = new MySqlConnection(_dbConection);
+                connection.Open();
+                Console.WriteLine("Sucesso na Conexão");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Falha na conexão, tente novamente" + erro.Message);
+            }
+        }
+
+    }
+}
---
+++ Conceitos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conceitos
+{
+    class Program
+    {
+        //Objeto de conexão
+        static void Main(string[] args)
+        {
+            DbConection conection = new();
+            conection.TestConnection();
+        }
+
+    }
+}
